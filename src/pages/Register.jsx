672ee39a6b99403b9404950866f4172f53e6c606
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-12-12 01:35:43 +0600

    firebase added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,33 @@
+import Lottie from 'lottie-react';
 import React from 'react';
+import LottieRegister from '../assets/Animation - 1733940535407.json'
 
 const Register = () => {
+
+
+    const handelRegister =e=>{
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const email =form.email.value;
+        const password = form.password.value;
+        const photo = form.photo.value;
+        const info ={
+            name , email, password, photo
+        }
+
+        console.log(info);
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
+                <div className="text-center lg:text-left w-full max-w-md ">
+                    <Lottie animationData={LottieRegister}></Lottie>
                 </div>
                 <div className="card bg-base-100 w-full p-6 max-w-md shrink-0 shadow-2xl">
                     <h1 className="text-5xl font-bold">Register now!</h1>
 
-                    <form className="card-body">
+                    <form onSubmit={handelRegister} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
@@ -24,13 +38,13 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
